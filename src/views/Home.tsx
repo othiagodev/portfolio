331c57e8d93e6be6698e6dfe50338a8ce10989f8
clust3rr <thg.santos@outlook.com>
2020-10-26 17:09:39 -0300

    configured project

---
+++ src/views/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/views/home.css';
+
+function Home() {
+  return (
+    <div id="home">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default Home;
