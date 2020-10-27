e098b2a62e8652c3c27beb2b025dbc6d984cbb19
clust3rr <thg.santos@outlook.com>
2020-10-26 22:45:00 -0300

    header test

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,12 +1,42 @@
+/* eslint-disable arrow-body-style */
 import React from 'react';
 import '../styles/views/home.css';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
 
-function Home() {
+export default function Home() {
+  const socialNetworkData = [
+    {
+      name: 'github',
+      url: 'https://github.com/othiagodev',
+      icon: (<FaGithub size={32} color="#FFF" />),
+    },
+    {
+      name: 'linkedin',
+      url: 'https://www.linkedin.com/othiagodev',
+      icon: (<FaLinkedinIn size={32} color="#FFF" />),
+    },
+  ];
   return (
     <div id="home">
-      <h1>Hello World</h1>
+      <header className="header">
+        <div className="my-name">
+          <span>Thiago</span>
+        </div>
+        <nav className="social-network">
+          <ul>
+            <li>
+              {socialNetworkData.map((data) => {
+                return (
+                  <a className={data.name} href={data.url}>{data.icon}</a>
+                );
+              })}
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="red" />
+      <div className="green" />
+      <div className="blue" />
     </div>
   );
 }
-
-export default Home;
