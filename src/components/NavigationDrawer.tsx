deaa1d14e2a02aac736954dc0efbebfb1ff45f10
clust3rr <thg.santos@outlook.com>
2021-05-07 21:40:54 -0300

    added nav content

--- src/components/NavigationDrawer.tsx
+++ src/components/NavigationDrawer.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
 import '../styles/components/navigationDrawer.css';
 
 let isOn = false;
@@ -18,7 +20,63 @@ export default function NavigationDrawer() {
     <div id="navigation">
       <div className="nav-container">
         <div className="nav-content">
-          <div className="content-view"><div>nav</div></div>
+          <div className="content-view">
+            <div>switch</div>
+            <hr />
+            <div>
+              <ul>
+                <li>
+                  <div className="link-content">
+                    <a
+                      className="social-network-link mail"
+                      href="mailto:thg.santos@outlook.com"
+                    >
+                      <div className="link-icon">
+                        <FiMail />
+                      </div>
+                      <div className="link-name">
+                        <p>Email</p>
+                      </div>
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="link-content">
+                    <a
+                      className="social-network-link github"
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://github.com/othiagodev"
+                    >
+                      <div className="link-icon">
+                        <FaGithub />
+                      </div>
+                      <div className="link-name">
+                        <p>GitHub</p>
+                      </div>
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <div className="link-content">
+                    <a
+                      className="social-network-link linkedin"
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://www.linkedin.com/in/othiagodev/"
+                    >
+                      <div className="link-icon">
+                        <FaLinkedinIn />
+                      </div>
+                      <div className="link-name">
+                        <p>Linkedin</p>
+                      </div>
+                    </a>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
         <div className="nav-ornament">
           <div className="ornament-view" />
